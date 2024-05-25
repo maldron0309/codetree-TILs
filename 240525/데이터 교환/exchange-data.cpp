802c320f821acef5a8889f50a50259542036b6eb
Maldron7 <bagjinhyeong640@gmail.com>
2024-05-25 20:32:04 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240525/데이터 교환/exchange-data.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 6, c = 7;
+    int temp = a; 
+
+    a = c; 
+    c = b;
+    b = temp; 
+
+    cout << a << '\n' << b << '\n' << c;
+    return 0;
+}
