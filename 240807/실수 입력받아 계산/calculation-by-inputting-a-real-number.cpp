2099bf855cb288ce8e84e655cab3c126eaa5c496
Maldron <bagjinhyeong640@gmail.com>
2024-08-07 14:24:29 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240807/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    double a{}, b{};
+    std::cin >> a >> b;
+    std::cout << std::fixed << std::setprecision(2) << (a + b);
+
+    return 0;
+}
