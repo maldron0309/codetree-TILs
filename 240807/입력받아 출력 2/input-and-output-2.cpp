02bb2a4a93a547ba605486f449e88078e9d68fb2
Maldron <bagjinhyeong640@gmail.com>
2024-08-07 15:05:00 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240807/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int front{}, back{};
+
+    std::cin >> front;
+    std::cin.get();
+    std::cin >> back;
+
+    std::cout << front << back;
+    return 0;
+}
