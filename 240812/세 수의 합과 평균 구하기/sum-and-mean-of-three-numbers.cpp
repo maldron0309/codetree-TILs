fa88f0fe7f8f27aee0b4a5908b8a247f15e9636c
Maldron <bagjinhyeong640@gmail.com>
2024-08-12 10:57:06 +0900

    Title: 세 수의 합과 평균 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240812/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+    int a{}, b{}, c{}, sum{}, avg{};
+    std::cin >> a >> b >> c;
+
+    sum = (a + b + c);
+    avg = (a + b + c) / 3;
+
+    std::cout << sum << '\n' << avg;
+
+    return 0;
+}
