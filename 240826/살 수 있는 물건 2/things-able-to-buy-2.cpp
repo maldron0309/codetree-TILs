00c749a25139347d25af301d894e52fb53ae3eda
Maldron <bagjinhyeong640@gmail.com>
2024-08-26 21:52:58 +0900

    Title: 살 수 있는 물건 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240826/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() 
+{
+    int n{};
+    std::cin >> n;
+
+    if (n >= 3000)
+    {
+        std::cout << "book";
+    }
+    else if (n >= 1000)
+    {
+        std::cout << "mask";
+    }
+    else 
+    {
+        std::cout << "no";
+    }
+
+    return 0;
+}
