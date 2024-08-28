2376cd0ac8832da25e7b10b48bca2cb937f96951
Maldron <bagjinhyeong640@gmail.com>
2024-08-28 11:16:21 +0900

    Title: 굉장한 숫자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240828/굉장한 숫자/amazing-number.cpp
+++ 240828/굉장한 숫자/amazing-number.cpp
@@ -5,7 +5,7 @@ int main()
     int n{};
     std::cin >> n;
 
-    std::string str = ((n % 3 == 0) && (n % 5 == 0)) ? "true":"false";
+    std::string str = ((n % 2 == 1) && (n % 3 == 0) || ((n % 2 == 0) && (n % 5 == 0))) ? "true":"false";
     std::cout << str;
 
     return 0;
